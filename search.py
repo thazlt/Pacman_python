f740afab3353c020d2f04a62058fdf1824b27a85
Thach Lai <1859044@itec.hcmus.edu.vn>
2021-02-25 16:47:23 +0700

    init

---
+++ search.py
@@ -0,0 +1,105 @@
+"""
+In search.py, you will implement generic search algorithms which are called by
+Pacman agents (in searchAgents.py).
+"""
+
+import util
+import sys
+from time import sleep
+
+from game import Directions
+
+n = Directions.NORTH
+s = Directions.SOUTH
+e = Directions.EAST
+w = Directions.WEST
+
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+def depthFirstSearch(problem):
+    '''
+    return a path to the goal
+    '''
+    # TODO 05
+
+
+def breadthFirstSearch(problem):
+    '''
+    return a path to the goal
+    '''
+    # TODO 06
+
+
+def uniformCostSearch(problem):
+    '''
+    return a path to the goal
+    '''
+    # TODO 07
+
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+# TODO 08 + 09
+'''
+students propose at least two heuristic functions for A*
+'''
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+    '''
+    return a path to the goal
+    '''
+    # TODO 10
+
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
